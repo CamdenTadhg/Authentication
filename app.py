7edfbe58acbde897699563b2da2f543a10c461e4
camdentadhg <camdent@gmail.com>
2024-03-05 09:46:36 -0600

    user authentication routes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
 
@@ -11,27 +12,74 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = 'requin'
-app.config["DEBUG_TB_INTERCEPT_REDIRECT"] = False
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 connect_db(app)
 db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
-# 16 make user routes (/, /register GET, /register POST, /login GET, /login POST, /secret)
-# 15 limit access to /secret
-# 14 make logout route
-# 13 change /secret route to a user page
-# 12 create feedback model
-# 11 make routes for feedback
-# 10 make additional routes for users (/users/username, /users/username/delete, users/username/feedback/add GET, users/username/feedback/add POST, /feedback/feedbackid/update GET, /feedback/feedbackid/update POST, /feedback/feedbackid/delete)
+@app.route('/')
+def route_register():
+    return redirect('/register')
 
-# 9 make sure registration and authentication are methods on your user class
-# 8 if there is already a username in session, do not allow users to see register or login forms
-# 7 add a 404 page and a 401 page when users are not authenticated
-# 6 add column to user table for admins. Admines can add, update, or delete any feedback and delete users
-# 5 if form submissions fail, display helpful error messages about what went wrong
-# 4 add tests for all view functions and model methods
-# 3 validate and confirm password
+@app.route('/register', methods=["GET", "POST"])
+def register_user():
+    """displays registration form and registers new users"""
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        new_user = User.register(username, password, email, first_name, last_name)
+        db.session.add(new_user)
+        db.session.commit()
+        session['username'] = new_user.username
+        flash('Welcome, Successfully created account', 'success')
+        return redirect('/secret')
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=["GET", "POST"])
+def login_user():
+    """displays login form and logs in authorized users"""
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User.authenticate(username, password)
+        if user: 
+            session["username"] = user.username
+            return redirect('/secret')
+        else:
+            form.username.errors=['invalid username/password']
+    return render_template('login.html', form=form)
+
+@app.route('/secret')
+def display_secret():
+    """displays secret page to authorized users"""
+    if "username" not in session: 
+        flash("Please register or login first!", "danger")
+        return redirect('/')
+    return render_template('secret.html')
+
+# 16 limit access to /secret
+# 15 make logout route
+# 14 change /secret route to a user page
+# 13 create feedback model
+# 12 make routes for feedback
+# 11 make additional routes for users (/users/username, /users/username/delete, users/username/feedback/add GET, users/username/feedback/add POST, /feedback/feedbackid/update GET, /feedback/feedbackid/update POST, /feedback/feedbackid/delete)
+
+# 10 make sure registration and authentication are methods on your user class
+# 9 if there is already a username in session, do not allow users to see register or login forms
+# 8 add a 404 page and a 401 page when users are not authenticated
+# 7 add column to user table for admins. Admines can add, update, or delete any feedback and delete users
+# 6 if form submissions fail, display helpful error messages about what went wrong
+# 5 add tests for all view functions and model methods
+# 4 validate and confirm password
+# 3 differentiate between invalid username and invalid password
 # 2 add functionality to reset a password
 # 1 style it
--- models.py
+++ models.py
@@ -19,3 +19,26 @@ class User(db.Model):
     email = db.Column(db.Text(length=50), nullable=False)
     first_name = db.Column(db.Text(length=30), nullable=False)
     last_name = db.Column(db.Text(length=30), nullable=False)
+
+    @classmethod
+    def register(cls, username, pwd, email, first_name, last_name):
+        """Register user w/ hashed password and return user"""
+
+        hashed = bcrypt.generate_password_hash(pwd)
+        # turn byte string into normal string
+        hashed_utf8 = hashed.decode('utf8')
+
+        #return instance of user w/ username and hashed password
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+    
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Validate that a user exists, have given the right password. Return user if valid, else false"""
+
+        u = User.query.filter_by(username=username).first()
+
+        if u and bcrypt.check_password_hash(u.password, pwd):
+            #return user instance
+            return u
+        else:
+            return False
