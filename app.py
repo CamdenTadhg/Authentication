1e71cd28912a33f6c3840c81c9519084a2da04fd
camdentadhg <camdent@gmail.com>
2024-03-05 07:40:41 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+
+
+
+
+# 18 set up the environment (install, venv, git, import, set up flask app)
+# 17 create user model
+# 16 create base template
+# 15 make user routes (/, /register GET, /register POST, /login GET, /login POST, /secret)
+# 14 limit access to /secret
+# 13 make logout route
+# 12 change /secret route to a user page
+# 11 create feedback model
+# 10 make routes for feedback
+# 9 make additional routes for users (/users/username, /users/username/delete, users/username/feedback/add GET, users/username/feedback/add POST, /feedback/feedbackid/update GET, /feedback/feedbackid/update POST, /feedback/feedbackid/delete)
+
+# 8 make sure registration and authentication are methods on your user class
+# 7 if there is already a username in session, do not allow users to see register or login forms
+# 6 add a 404 page and a 401 page when users are not authenticated
+# 5 add column to user table for admins. Admines can add, update, or delete any feedback and delete users
+# 4 if form submissions fail, display helpful error messages about what went wrong
+# 3 add tests for all view functions and model methods
+# 2 add functionality to reset a password
+# 1 style it
